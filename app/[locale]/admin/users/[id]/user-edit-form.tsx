226ec7088dd127178b3384d7139d252292d6d532
viora-dev <sarakia2077@gmail.com>
2026-07-11 23:28:17 +0430

    fix user object id type

--- app/[locale]/admin/users/[id]/user-edit-form.tsx
+++ app/[locale]/admin/users/[id]/user-edit-form.tsx
@@ -39,7 +39,10 @@ const UserEditForm = ({ user }: { user: IUser }) => {
 
   const form = useForm<z.infer<typeof UserUpdateSchema>>({
     resolver: zodResolver(UserUpdateSchema),
-    defaultValues: user,
+    defaultValues: {
+  ...user,
+  _id: user._id.toString(),
+},
   })
 
   const { toast } = useToast()
@@ -48,7 +51,7 @@ const UserEditForm = ({ user }: { user: IUser }) => {
     try {
       const res = await updateUser({
         ...values,
-        _id: user._id,
+         _id: user._id.toString(),
       })
 
       if (!res.success) {
