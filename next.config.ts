2da3683144725061f24f8ddd24dfd1f480d5d4c2
viora-dev <sarakia2077@gmail.com>
2026-07-20 19:23:56 +0430

    Fix Liara deployment

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts')
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+
   eslint: {
     ignoreDuringBuilds: true,
   },
