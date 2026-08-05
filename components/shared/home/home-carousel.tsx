ed9067015693aabd8a1c7ebc799d1db8bd4e0d9c
viora-dev <sarakia2077@gmail.com>
2026-08-05 19:36:23 +0430

    fix: fill desktop carousel images

--- components/shared/home/home-carousel.tsx
+++ components/shared/home/home-carousel.tsx
@@ -64,7 +64,7 @@ export function HomeCarousel({ items }: { items: ICarousel[] }) {
                   fill
                   priority
                   sizes='100vw'
-                  className='object-contain object-center'
+                  className='object-contain object-center lg:object-cover'
                 />
 
                 <div className='absolute inset-y-0 left-0 w-1/2 bg-gradient-to-r from-white/55 to-transparent' />
